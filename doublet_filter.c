8ee76de58f14fd3635568852a6792b317939bc92
Stefan May <smay@4finger.net>
2015-03-05 12:56:41 +0100

    relicensed under GPLv3

--- doublet_filter.c
+++ doublet_filter.c
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2015  Stefan May <smay@4finger.net>
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdbool.h>
@@ -27,7 +43,7 @@ inline static void* emalloc(size_t size) {
     return ptr;
 }
 
-inline static void* emalloc_copy(void* src, size_t size) 
+inline static void* emalloc_copy(void* src, size_t size)
 {
     void* ptr = emalloc(size);
     return strncpy(ptr, src, size);
